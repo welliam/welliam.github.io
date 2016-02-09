37d7c345d5509c4e6df24c58c2b5eb918013ec17
Justin Lange <well1912@gmail.com>
2016-02-08 16:29:15 -0800

    Do not allow multiple scales to be played at once

--- play.js
+++ play.js
@@ -1,36 +1,24 @@
 (function () {
-    function playNext(notes, s, i) {
-        return function () {
-            if (i >= 12) {
-                notes[12].play()
-            } else if ((s >> i) & 1) {
-                notes[i].onended = playNext(notes, s, i+1)
-                notes[i].play()
-            } else {
-                playNext(notes, s, i+1)()
+    var audio = new Audio()
+
+    function play(s, i) {
+        if (i == 12) {
+            audio.src = 'resources/12.ogg'
+            audio.onended = function() {
             }
+            audio.play()
+        } else if ((s >> i) & 1) {
+            audio.src = 'resources/' + i + '.ogg'
+            audio.onended = function() {
+                play(s, i+1)
+            }
+            audio.play()
+        } else {
+            play(s, i+1)
         }
     }
-
-    function startPlaying(notes, s) {
-        playNext(notes, s, 0)()
-    }
-
-    var noteElements = false
-
-    function getNotes() {
-        if (!noteElements) {
-            noteElements = []
-            for(var i = 0; i < 13; i++) {
-                noteElements.push(document.getElementById('note' + i))
-            }
-        }
-        return noteElements
-    }
-
-    var playing = false
 
     playScale = function(s) {
-        startPlaying(getNotes(), s)
+        play(s, 0)
     }
 })()
