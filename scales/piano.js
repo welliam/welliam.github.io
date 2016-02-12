378fc16d298be4996e4977fbf3c0819ebc63fb44
Justin Lange <well1912@gmail.com>
2016-02-12 09:16:23 -0800

    Package scripts into single .js file
    * Compartmentalize functions into "packages" (immediately evaluated
      function expressions which add functions to PIANO object)
    * Remove additional scripts

--- scales/piano.js
+++ scales/piano.js
@@ -1,11 +1,71 @@
-(function () {
+PIANO = {}; // used for globally-visible functions
 
+
+// scales
+
+(function() {
+    function defscale(name, notes) {
+        return {
+            name: name,
+            notes: parseInt(notes, 2)
+        }
+    }
+
+    var scales = [
+        // major and modes
+        defscale('Major', '101010110101'),
+        defscale('Natural Minor', '010110101101'),
+        defscale('Ionian', '101010110101'),
+        defscale('Dorian', '011010101101'),
+        defscale('Phrygian', '010110101011'),
+        defscale('Lydian', '101011010101'),
+        defscale('Mixolydian', '011010110101'),
+        defscale('Aeolian', '010110101101'),
+        defscale('Locrian', '010101101011'),
+
+        // melodic minor and modes
+        defscale('Melodic Minor', '101010101101'),
+        defscale('Dorian b2', '011010101011'),
+        defscale('Lydian #5', '101101010101'),
+        defscale('Lydian Dominant', '011011010101'),
+        defscale('Aeolian Dominant', '010110110101'),
+        defscale('Half Diminished', '010101101101'),
+        defscale('Altered', '010101011011'),
+
+        defscale('Harmonic Minor', '100110101101'),
+
+        // Named MOLTs
+        defscale('Chromatic', '111111111111'),
+        defscale('Augmented', '100110011001'),
+        defscale('Tritone', '010011010011'),
+        defscale('Whole Tone', '010101010101'),
+        defscale('Diminished 1', '011011011011'),
+        defscale('Diminished 2', '101101101101'),
+        defscale('MOLT 3', '110111011101'),
+        defscale('MOLT 4', '100111100111'),
+        defscale('MOLT 5', '100011100011'),
+        defscale('MOLT 6', '110011110011'),
+        defscale('MOLT 7', '101111101111'),
+
+        // Pentatonic scales and friends
+        defscale('Major Pentatonic', '001010010101'),
+        defscale('Minor Pentatonic', '010010101001'),
+        defscale('Blues', '010011101001')
+    ]
+
+    PIANO.scalesDictionary = scales
+})();
+
+
+// making the keyboard
+
+(function() {
     function isBlack(nth) {
         switch(nth % 12) {
         case 0: case 2: case 4: case 5: case 7: case 9: case 11:
-            return false;
+            return false
         default:
-            return true;
+            return true
         }
     }
 
@@ -69,6 +129,21 @@
         return keys
     }
 
+    PIANO.makeKeys = makeKeys
+})();
+
+
+// scale predicates
+
+(function () {
+    function hammingDistance(s1notes, s2notes) {
+        var sum = 0
+        for(s1notes ^= s2notes; s1notes > 0; s1notes >>= 1) {
+            sum += s1notes & 1
+        }
+        return sum
+    }
+
     function areModes(s1, s2) {
         if (s1 == s2) {
             return false
@@ -85,20 +160,34 @@
         return false
     }
 
-    function hammingDistance(s1notes, s2notes) {
-        var sum = 0
-        for(s1notes ^= s2notes; s1notes > 0; s1notes >>= 1) {
-            sum += s1notes & 1
-        }
-        return sum
-    }
-
     function sortScales(notes, scales) {
         return scales.sort(function (s1, s2) {
             var hd1 = hammingDistance(notes, s1.notes),
                 hd2 = hammingDistance(notes, s2.notes)
             return hd1 == hd2 ? s1.notes > s2.notes : hd1 > hd2
         })
+    }
+
+    function isMolt(s) {
+        if (!s % 2) {
+            return false
+        }
+
+        return s % 64 == s >> 6 ||
+            (s % 16 == (s >> 4) % 16 && s % 16 == s >> 8)
+    }
+
+    function isSymmetrical(s) {
+        if (!s % 2) {
+            return false
+        }
+        s >>= 1
+        var rev = 0
+        for(var i = 0; i < 11; i++) {
+            rev *= 2
+            rev += (s >> i) % 2
+        }
+        return rev == s
     }
 
     function similarScales(s1notes, scales) {
@@ -129,6 +218,68 @@
         }
     }
 
+    PIANO.similarScales = similarScales
+    PIANO.isMolt = isMolt
+    PIANO.isSymmetrical = isSymmetrical
+})();
+
+
+// playing notes
+
+(function () {
+    var wasSetup = false, notes = []
+
+    function setup() {
+        if (!wasSetup) {
+            for(var i = 0; i < 13; i++) {
+                var a = new Audio()
+                a.src = 'resources/' + i + '.ogg'
+                a.type = 'audio/ogg'
+                notes.push(a)
+            }
+            wasSetup = true
+        }
+    }
+
+    function stop() {
+        setup()
+        notes.forEach(function(a) {
+            a.pause()
+            a.currentTime = 0
+        })
+    }
+
+    function note(i, next) {
+        next = next || function() {}
+        notes[i].onended = next
+        notes[i].play()
+    }
+
+    function play(s, i) {
+        stop() // calls setup
+
+        if (i == 12) {
+            note(i)
+        } else if ((s >> i) & 1) {
+            note(i, function() {
+                play(s, i+1)
+            })
+        } else {
+            play(s, i+1)
+        }
+    }
+
+    PIANO.playScale = function(s) {
+        play(s, 0)
+    }
+
+    PIANO.stopPlayScale = stop
+})();
+
+
+// IO functions
+
+(function () {
     function scaleLink(name, s) {
         var link = document.createElement('a')
         link.setAttribute('href', '#')
@@ -160,34 +311,12 @@
         document.getElementById('statslist').appendChild(li)
     }
 
-    function isMolt(s) {
-        if (!s % 2) {
-            return false
-        }
-
-        return s % 64 == s >> 6 ||
-            (s % 16 == (s >> 4) % 16 && s % 16 == s >> 8)
-    }
-
-    function isSymmetrical(s) {
-        if (!s % 2) {
-            return false
-        }
-        s >>= 1
-        var rev = 0
-        for(var i = 0; i < 11; i++) {
-            rev *= 2
-            rev += (s >> i) % 2
-        }
-        return rev == s
-    }
-
     function tellAllStats(s) {
-        if (isSymmetrical(s)) {
+        if (PIANO.isSymmetrical(s)) {
             statsTell('This scale is symmetrical.')
         }
 
-        if (isMolt(s)) {
+        if (PIANO.isMolt(s)) {
             statsTell('This scale is a mode of limited transposition.')
         }
     }
@@ -199,7 +328,7 @@
         document.getElementById('statslist').innerHTML = ''
         tellAllStats(s)
 
-        var similars = similarScales(s, scaledict)
+        var similars = PIANO.similarScales(s, scaledict)
         resetScales(document.getElementById('same'),
                     document.getElementById('samelist'),
                     similars.same)
@@ -264,7 +393,7 @@
         var scale = 0,
             piano = document.getElementById('piano')
 
-        keys = makeKeys(piano, function(n) {
+        keys = PIANO.makeKeys(piano, function(n) {
             scale ^= 1 << n
             updateScalenum(scale)
         })
@@ -316,4 +445,4 @@
             style.display = x
         })
     }
-})()
+})();
