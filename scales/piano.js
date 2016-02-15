a4594dd9353a12683adc9f6642c9c41b1b270f04
Justin Lange <well1912@gmail.com>
2016-02-14 19:52:37 -0800

    Add revert function, other small edits
    * rename deleteScale to removeScale
    * pack scales functions as into PIANO as object named `scales`

--- scales/piano.js
+++ scales/piano.js
@@ -15,44 +15,48 @@ PIANO = {}; // used for globally-visible functions
     }
 
     // major and modes
-    defscale('Major', '101010110101'),
-    defscale('Natural Minor', '010110101101'),
-    defscale('Ionian', '101010110101'),
-    defscale('Dorian', '011010101101'),
-    defscale('Phrygian', '010110101011'),
-    defscale('Lydian', '101011010101'),
-    defscale('Mixolydian', '011010110101'),
-    defscale('Aeolian', '010110101101'),
-    defscale('Locrian', '010101101011'),
+    function revert() {
+        defscale('Major', '101010110101'),
+        defscale('Natural Minor', '010110101101'),
+        defscale('Ionian', '101010110101'),
+        defscale('Dorian', '011010101101'),
+        defscale('Phrygian', '010110101011'),
+        defscale('Lydian', '101011010101'),
+        defscale('Mixolydian', '011010110101'),
+        defscale('Aeolian', '010110101101'),
+        defscale('Locrian', '010101101011'),
 
-    // melodic minor and modes
-    defscale('Melodic Minor', '101010101101'),
-    defscale('Dorian b2', '011010101011'),
-    defscale('Lydian #5', '101101010101'),
-    defscale('Lydian Dominant', '011011010101'),
-    defscale('Aeolian Dominant', '010110110101'),
-    defscale('Half Diminished', '010101101101'),
-    defscale('Altered', '010101011011'),
+        // melodic minor and modes
+        defscale('Melodic Minor', '101010101101'),
+        defscale('Dorian b2', '011010101011'),
+        defscale('Lydian #5', '101101010101'),
+        defscale('Lydian Dominant', '011011010101'),
+        defscale('Aeolian Dominant', '010110110101'),
+        defscale('Half Diminished', '010101101101'),
+        defscale('Altered', '010101011011'),
 
-    defscale('Harmonic Minor', '100110101101'),
+        defscale('Harmonic Minor', '100110101101'),
 
-    // Named MOLTs
-    defscale('Chromatic', '111111111111'),
-    defscale('Augmented', '100110011001'),
-    defscale('Tritone', '010011010011'),
-    defscale('Whole Tone', '010101010101'),
-    defscale('Diminished 1', '011011011011'),
-    defscale('Diminished 2', '101101101101'),
-    defscale('MOLT 3', '110111011101'),
-    defscale('MOLT 4', '100111100111'),
-    defscale('MOLT 5', '100011100011'),
-    defscale('MOLT 6', '110011110011'),
-    defscale('MOLT 7', '101111101111'),
+        // Named MOLTs
+        defscale('Chromatic', '111111111111'),
+        defscale('Augmented', '100110011001'),
+        defscale('Tritone', '010011010011'),
+        defscale('Whole Tone', '010101010101'),
+        defscale('Diminished 1', '011011011011'),
+        defscale('Diminished 2', '101101101101'),
+        defscale('MOLT 3', '110111011101'),
+        defscale('MOLT 4', '100111100111'),
+        defscale('MOLT 5', '100011100011'),
+        defscale('MOLT 6', '110011110011'),
+        defscale('MOLT 7', '101111101111'),
 
-    // Pentatonic scales and friends
-    defscale('Major Pentatonic', '001010010101'),
-    defscale('Minor Pentatonic', '010010101001'),
-    defscale('Blues', '010011101001');
+        // Pentatonic scales and friends
+        defscale('Major Pentatonic', '001010010101'),
+        defscale('Minor Pentatonic', '010010101001'),
+        defscale('Blues', '010011101001');
+    }
+
+    revert()
 
     function getAllScales() {
         var t = [];
@@ -65,13 +69,16 @@ PIANO = {}; // used for globally-visible functions
         return t;
     }
 
-    function deleteScale(name) {
+    function removeScale(name) {
         scales[name] = null;
     }
 
-    PIANO.addScale = addScale;
-    PIANO.getAllScales = getAllScales;
-    PIANO.deleteScale = deleteScale;
+    PIANO.scales = {
+        add: addScale,
+        get: getAllScales,
+        remove: removeScale,
+        revert: revert
+    };
 })();
 
 
@@ -377,7 +384,7 @@ PIANO = {}; // used for globally-visible functions
         document.getElementById('scalenum').value = scale;
         document.getElementById('statslist').innerHTML = '';
         tellAllStats(scale);
-        refreshAllScales(keys, scale, PIANO.getAllScales());
+        refreshAllScales(keys, scale, PIANO.scales.get());
     }
 
     function clearKeys(keys) {
@@ -419,8 +426,8 @@ PIANO = {}; // used for globally-visible functions
     function addScale(keys, scale) {
             var title = document.getElementById('title').value
             if (title) {
-                PIANO.addScale(title, scale);
-                refreshAllScales(keys, scale, PIANO.getAllScales());
+                PIANO.scales.add(title, scale);
+                refreshAllScales(keys, scale, PIANO.scales.get());
             }
     }
 
